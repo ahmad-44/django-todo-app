3872e01c6a7b1ff578fc3aaa6eb6c8560affa6d8
Ahmad Ali <ahmadalibaffa@gmail.com>
2023-12-09 02:02:52 +0500

    Created Model for Tasks and added User as a foreign key, then completed the DB migration. Tables are set in the DB now

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User, UserManager
+
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    desc = models.TextField()
+    is_Task_Completed = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
