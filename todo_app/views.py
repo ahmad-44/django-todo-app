e8bf4e65061deda21b2685442ef8ad586f8e545f
Ahmad Ali <ahmadalibaffa@gmail.com>
2023-12-09 03:05:52 +0500

    Now I am able to print the values of tasks on template. Problem is that the ID of user of that task is hard coded into view. It should become dynamic in upcoming commits

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Task
+from django.contrib.auth.models import User
+
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {'name': 'ahmad'})
+    # fetch data in form of queryset
+    tasks = Task.objects.filter(user_id='2').values()
+
+    # convert that data into a dict
+    my_dict = {}
+
+    for index, element in enumerate(tasks):
+        my_dict[index] = element
+
+
+    return render(request, 'index.html', {'tasks': tasks})
