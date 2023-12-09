05f15ba2b177ddc6ad0608b6ae4da8b10699e483
Ahmad Ali <ahmadalibaffa@gmail.com>
2023-12-09 07:30:24 +0500

    Login page and login created, a minor URLs bug is fixed which was shoiwing dual directories like accounts/login/login. In urls.py gave a name like login to accounts/login. then used {% url 'login' %} in template to fix the issue of duplicaiton of directory.

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("register", views.register, name="register"),
+    path("accounts/register", views.register, name="register"),
+    path("accounts/login", views.login, name="login"),
     path("logout", views.logout, name="logout")
 ]
